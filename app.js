15a5038fad9e33db132cf6b7052412c6ea172215
peggiezx <56562224+peggiezx@users.noreply.github.com>
2021-09-14 17:47:45 -0400

    make it better

--- app.js
+++ app.js
@@ -111,7 +111,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err})
 })
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("SERVING ON PORT 3000")
+app.listen(port, () => {
+    console.log(`SERVING ON PORT ${port}`)
 })
